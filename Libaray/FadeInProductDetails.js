734f4aa6afafc06b9e111dd0383350f151dae21d
zeynab335 <zeynab335>
2022-12-24 19:29:06 +0200

    add Hover in all items in Products that are Trending now

--- Libaray/FadeInProductDetails.js
+++ Libaray/FadeInProductDetails.js
@@ -2,16 +2,40 @@
 
 //* 
 $(function(){
-    var products = $('.view-products .carousel-inner-item .product_details img');
+    var products = $('.view-products .carousel-inner-item ');
     for (var i = 0; i < products.length; i++) {
-        products.eq(i).mouseover(function(){
-            products.eq(i).prev().css('display' , 'block')
-        }
-               
-            
-        )
+        if(products.eq(i).hover)
+        products.eq(i).hover(function(){
+            //* Display Overlay 
+            $(this).children(0).children(0).eq(0).addClass('active',1000) 
+
+            //* Display Button of btn-carousel-view-details
+            $(this).children(1).addClass('hover',1000) 
+        },
+        function(){
+            //* Display Overlay 
+            $(this).children(0).children(0).eq(0).removeClass('active')
+
+            //* Display Button of btn-carousel-view-details
+            $(this).children(1).next().removeClass('hover')  
+        })
+        
     }
+
+
+    // products.eq(0).hover(function(){
+    //     //* Display Overlay 
+
+    //     //* Display Button of btn-carousel-view-details
+    //     $(this).parent().next().addClass('hover') 
+    // },
+    // function(){
+    //     //* Display Button of btn-carousel-view-details
+    //     $(this).parent().next().removeClass('hover')  
+    // })
 })
         
+ 
+
 
     
