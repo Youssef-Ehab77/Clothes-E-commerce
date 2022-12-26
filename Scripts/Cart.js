347c76b69b289ba632b963adc852a2de96ffe6f4
Omar Mosaad <omarmosaad2009@gmail.com>
2022-12-26 22:55:18 +0200

    ongoing...

--- Scripts/Cart.js
+++ Scripts/Cart.js
@@ -1,14 +1,6 @@
 $(function() {
-    var count = 0;
-    $("#increment").click(function() {
-      count++;
-      $("#count").text(count);
-    });
-    $("#decrement").click(function() {
-        if (count != 0)
-            count--;
-      $("#count").text(count);
-    });
+
+    var number_of_items = 0;
 
     $.ajax("../json/Products.json", { //open the json file
         type:"GET",
@@ -19,6 +11,8 @@ $(function() {
 
             for (item of dataObj)
             {
+                number_of_items += 1;
+
                 $(".addOn").append(
                 
             "<div class='product'>" +
@@ -27,20 +21,65 @@ $(function() {
                 "<div class='product-detail'>Black Trouser</div>" +
             "</div>" +
             "<div class='quantity-blocks'>" +
-                "<button id='decrement' class='incdec'>-</button>" +
-                "<div id='count' class='product-detail'>0</div>" +
-                "<button id='increment' class='incdec'>+</button>" +
+                "<button class='decrement' class='incdec'>-</button>" +
+                "<div class='count' class='product-detail'>1</div>" +
+                "<button class='increment' class='incdec'>+</button>" +
             "</div>"  +
 
-            "<div class='product-detail'>$54</div>" +
-            "<div class='product-detail'>$162</div>" +
+            "<div class='item-price' class='product-detail'>$54</div>" +
+            "<div class='item-total' class='product-detail'>$54</div>" +
         "</div>" +
         "<hr>")
             }
 
         }
-
     })
+
+    setTimeout(function (){
+        $('.number-items').text(number_of_items + ' items');
+
+        $(".increment").click(function() {
+            
+        var countElement = $(this).closest('.product').find('.count');
+        
+        var count = parseInt(countElement.text()) + 1;
+
+        var global_count = parseInt($('.number-items').text()) + 1
+        $('.number-items').text(global_count + 'items');
+
+        var price = ($(this).closest('.product').find('.item-price').text().slice(1));
+
+        var tmpTotal = count * price;
+     
+        $(this).closest('.product').find('.item-total').text('$' + tmpTotal) 
+
+        countElement.text(count);
+
+
+          
+        });
+        $(".decrement").click(function() {
+            var countElement = $(this).closest('.product').find('.count');
+            
+            if (parseInt(countElement.text()) != 0)
+            {
+                var count = parseInt(countElement.text()) -1;
+
+                var global_count = parseInt($('.number-items').text()) - 1
+                $('.number-items').text(global_count + ' items');
+
+                var price = ($(this).closest('.product').find('.item-price').text().slice(1));
+
+                var tmpTotal = count * price;
+             
+                $(this).closest('.product').find('.item-total').text('$' + tmpTotal) 
+            }
+            
+            countElement.text(count);
+        });
+}, 30)
+
+
 
 })
        
