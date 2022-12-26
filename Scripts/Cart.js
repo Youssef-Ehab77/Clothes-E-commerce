75b15a5e82be2db89014c5f36e3606d5ac30f09f
Omar Mosaad <omarmosaad2009@gmail.com>
2022-12-26 19:40:54 +0200

    added jquery

--- Scripts/Cart.js
+++ Scripts/Cart.js
@@ -9,4 +9,38 @@ $(function() {
             count--;
       $("#count").text(count);
     });
-  });
+
+    $.ajax("../json/Products.json", { //open the json file
+        type:"GET",
+        dataType:'json',
+        contentType: 'application/json',
+        success: function(dataObj)
+        {
+
+            for (item of dataObj)
+            {
+                $(".addOn").append(
+                
+            "<div class='product'>" +
+            "<div>" +
+                "<div class='product-image'><img src=" + item.src + "></div>" +
+                "<div class='product-detail'>Black Trouser</div>" +
+            "</div>" +
+            "<div class='quantity-blocks'>" +
+                "<button id='decrement' class='incdec'>-</button>" +
+                "<div id='count' class='product-detail'>0</div>" +
+                "<button id='increment' class='incdec'>+</button>" +
+            "</div>"  +
+
+            "<div class='product-detail'>$54</div>" +
+            "<div class='product-detail'>$162</div>" +
+        "</div>" +
+        "<hr>")
+            }
+
+        }
+
+    })
+
+})
+       
