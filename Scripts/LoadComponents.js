bcdb2af17aaf28f47bf2774a35d5c53f6064a1f1
zeynab335 <zeynab335>
2022-12-27 14:16:27 +0200

    load all components into index page

---
+++ Scripts/LoadComponents.js
@@ -0,0 +1,13 @@
+//* Load Components
+
+//* Load Header Component
+
+
+$(function() {
+    $("#header-component").load("../HTML/Header.html");
+});
+
+//* Load Product Component
+$(function() {
+    $("#product-component").load("../HTML/ViewProducts.html");
+});
