3d8f1e4d59217901ee59dc308b45f571f9a9d9c7
zeynab335 <zeynab335>
2022-12-28 22:41:24 +0200

    delete unused images in product images and make product loading dynamically

---
+++ Scripts/LoadProducts.js
@@ -0,0 +1,62 @@
+
+
+function getCategoryPath(category){
+    var path 
+    //* women category
+    if(category == 'women') {
+        path = "../../Data/Women.json";
+    }
+    //* men category
+    else if(category == 'men'){
+        path = "../../Data/Men.json";
+        
+    } 
+    //* Sale
+    else{
+        path = "../../Data/Men.json";
+    }
+    return path
+
+}
+
+function loadData(category){
+    var path = getCategoryPath(category)
+    //* Get data from json file
+    var xhr = new XMLHttpRequest();//{open}
+    //1-open
+    xhr.open("Get" , path);
+    
+    //3-event
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4){
+            if(xhr.status >=200 && xhr.status<300){
+                var data = JSON.parse(xhr.response).sort((a, b) => 0.5 - Math.random());;
+                for(var i=0; i<8;i++){
+                    //* append product items from json file into html file
+                    $('.carousel-inner').append(
+                        `
+                        <div class="carousel-inner-item">
+                            <div class="product_details">
+                                <div class="Overlay"></div>
+                                <img src='${data[i].path}' />            
+                            </div>
+                            <button class="btn-carousel-view-details">
+                                <a href = './Product.html?id=${data[i].id}' > View Details </a>
+                            </button>
+                        </div>  
+                        `
+                    )
+                }
+            }
+        }
+    }
+    //2-send
+    xhr.send("")
+}  
+
+
+$(function(){
+    loadData('women')
+})
+
+
