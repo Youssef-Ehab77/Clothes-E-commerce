abf3b949d026167a5a8d38bc246a35f683491e61
zeynab335 <zeynab335>
2022-12-28 13:36:28 +0200

    add fontawsome link to index html & change @media

---
+++ Scripts/ViewProducts.js
@@ -0,0 +1,17 @@
+var current = 0
+var start = 0
+var end = 7
+    document.querySelectorAll('.view-products .carousel-inner-item ').forEach(function(item){
+        var containerDimensions = item.getBoundingClientRect();
+        var containerWidth = containerDimensions.width;
+        console.log(item , containerDimensions )
+        
+        document.querySelector('.carousel-control-next').addEventListener('click', () => {
+            console.log("ss")
+            item.scrollLeft += containerWidth;
+        })
+    
+        document.querySelector('.carousel-control-prev').addEventListener('click', () => {
+            item.scrollLeft -= containerWidth;
+        })
+    })
