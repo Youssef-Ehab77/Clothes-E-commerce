4e9d90f1b1edea85b6e6912f2a1c56035f6721db
Youssef-Ehab77 <youssef.271998@gmail.com>
2022-12-28 21:15:13 +0200

    Cookies and Login/Register
    cookies are now ready to work
    login and registration are done

--- Scripts/Registration.js
+++ Scripts/Registration.js
@@ -1,13 +1,22 @@
-$('#sign-up-btn').click(function () {
-    let username = $('#username').get(0).value;
-    let password = $('#password').get(0).value;
+$(function () {
+    setTimeout(function () {
+        $('#sign-up-btn').click(function () {
+            let username = $('#r-username').get(0).value;
+            let password = $('#r-password').get(0).value;
 
-    if (username.length >= 4 && password.length >= 4) {
-        let user = {"username": username, "password": password};
-        let date = new Date();
-        date.setMonth(date.getMonth()+1)
-        document.cookie = "user="+user+"; expires="+date+";";
-    } else {
-        alert('Please Enter at least 4 characters!');
-    }
-})
+            if (username.length >= 4 && password.length >= 4) {
+                let date = new Date();
+                date.setMonth(date.getMonth() + 1)
+
+                setCookie('username', username, date);
+                setCookie('password', password, date);
+                if (confirm("Signed Up Successfully! \n Head To the Home Page Now")) {
+                    document.location.href = '../HTML/Index.html';
+                }
+
+            } else {
+                alert('Please Enter at least 4 characters!');
+            }
+        });
+    }, 300);
+});
