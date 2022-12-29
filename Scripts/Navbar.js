f423c177aef52c445d0eb726255b15ba7ad44fb4
zeynab335 <zeynab335>
2022-12-30 01:10:14 +0200

    add account link into home page and link pages with each other in navbar component

---
+++ Scripts/Navbar.js
@@ -0,0 +1,42 @@
+//* when click on logo => go back to home page
+$(function(){
+    $('#logo').click(function(){
+        history.back('./')
+    })
+})
+
+
+
+//* check if user login or not
+$(function(){
+    var isLogined = getCookie('username');
+    if(isLogined!==''){
+        $('#login span').text(isLogined);
+        $('.account-settings').get(0).children[0].innerText = 'Logout';
+        $('.account-settings').get(0).children[0].setAttribute ('id' , 'logout');
+
+        $('.account-settings').get(0).children[1].remove()
+        $('.account-settings').css('height','50px')
+    }
+    // $('.account-settings').get(0).children[0].setAttribute ('id' , '');
+
+})
+
+//* To fade in account settings
+$(function(){
+    $('#login').click(function(){
+        $('.account-settings').fadeToggle();
+        $('#login').toggleClass('activeLink');
+    })
+})
+
+//* logout handler
+$(function(){
+    $('#logout').click(function(){
+        console.log("logout")
+        deleteCookie('username')
+        deleteCookie('password')
+        location.reload()
+
+    })
+})
