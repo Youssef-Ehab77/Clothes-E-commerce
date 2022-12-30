6793c2e7e7563d321c3b65b683ca00c2fee6a0a4
zeynab335 <zeynab335>
2022-12-30 23:29:36 +0200

    add homepage button

---
+++ Scripts/Header.js
@@ -0,0 +1,11 @@
+//* view more items button handler
+$(function(){
+    setTimeout(function(){
+        console.log($('#btn-view-more'))
+        $('#btn-view-more').click(function(){
+            // $('.btn-view-more')
+        })
+        
+    },2000)
+    
+})
