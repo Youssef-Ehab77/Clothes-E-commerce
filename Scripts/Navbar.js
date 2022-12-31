c373b498be97eeefce88924c40628b903a0fd250
Youssef-Ehab77 <youssef.271998@gmail.com>
2022-12-31 21:25:42 +0200

    Update Navbar.js

--- Scripts/Navbar.js
+++ Scripts/Navbar.js
@@ -1,47 +1,47 @@
 //* when click on logo => go back to home page
-$(function(){
-    $('#logo').click(function(){
-        location.href = './Index.html'
+$(function () {
+    $('#logo').click(function () {
+        location.href = '../HTML/Index.html'
     })
 })
 
 // "/HTML/ContactUs.html"
-$(function(){
-    setTimeout(function(){
-        var pageName = location.pathname.substring(6,(location.pathname.length-5))
-        if(pageName){
-            $('#' + pageName).eq(0).addClass('active')
-            $('#' + pageName).eq(0).siblings().removeClass('active')
-        }
-    })
-    
-})
+$(function () {
+    var pageName = window.location.href;
+    pageName = pageName.split('/');
+    var length = pageName.length - 1;
+    pageName[length] = pageName[length].slice(0, -5);
+    // console.log(pageName[length]);
+    $('#' + pageName[length]).eq(0).addClass('active')
+    $('#' + pageName).eq(0).siblings().removeClass('active')
+
+});
 
 //* check if user login or not
-$(function(){
+$(function () {
     var isLogined = getCookie('username');
-    if(isLogined!==''){
+    if (isLogined !== '') {
         $('#login span').text(isLogined);
         $('.account-settings').get(0).children[0].innerText = 'Logout';
-        $('.account-settings').get(0).children[0].setAttribute ('id' , 'logout');
+        $('.account-settings').get(0).children[0].setAttribute('id', 'logout');
         $('.account-settings').get(0).children[1].remove()
-        $('.account-settings').css('height','50px')
+        $('.account-settings').css('height', '50px')
     }
     // $('.account-settings').get(0).children[0].setAttribute ('id' , '');
 
 })
 
 //* To fade in account settings
-$(function(){
-    $('#login').click(function(){
+$(function () {
+    $('#login').click(function () {
         $('.account-settings').fadeToggle();
         $('#login').toggleClass('activeLink');
     })
 })
 
 //* logout handler
-$(function(){
-    $('#logout').click(function(){
+$(function () {
+    $('#logout').click(function () {
         console.log("logout");
         location.reload();
 
